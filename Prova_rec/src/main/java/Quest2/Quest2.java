2a1d1958a444626188d930ae51c7ac448063f98b
Lian Henrique <lian.h.silva@aluno.senai.br>
2024-11-13 19:17:26 -0300

    questão 1

---
+++ Prova_rec/src/main/java/Quest2/Quest2.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Quest2;
+
+/**
+ *
+ * @author l.silva
+ */
+import java.util.Scanner;
+public class Quest2 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner (System.in);
+        System.out.println("Digite quantos anos voce tem: ");
+        int ano = s.nextInt();
+        System.out.println("Digite quantos meses tem (depois do ultimo aniversario): ");
+        int mes = s.nextInt();
+        System.out.println("Digite quantod dias tem (depois do ultimo aniversario): ");
+        int dia = s.nextInt();
+        int res = ano*365;
+        res += mes*30;
+        res += dia;
+        System.out.println("Seu tempo de vida em dias e "+res+" dias");
+    }
+}
